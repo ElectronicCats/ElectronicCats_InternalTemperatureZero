95f93c8c1c41376e8eda46ad2755fd5cf3000866
sabas1080 <s@theinventorhouse.org>
2018-11-26 13:49:01 -0600

    Fix example

---
+++ examples/ReadInternalTemperature/ReadInternalTemperature.ino.ino
@@ -0,0 +1,31 @@
+/***************************************************************************
+  This is a library for internal temperature of the family SAMD
+
+  Electronic Cats invests time and resources providing this open source code,
+  please support Electronic Cats and open-source hardware by purchasing products
+  from Electronic Cats!
+
+  Written by Andrés Sabas Electronic Cats.
+  This code is beerware; if you see me (or any other Electronic Cats 
+  member) at the local, and you've found our code helpful, 
+  please buy us a round!
+  Distributed as-is; no warranty is given.
+ ***************************************************************************/
+
+#include <TemperatureZero.h>
+
+TemperatureZero TempZero = TemperatureZero();
+
+void setup() {
+  // put your setup code here, to run once:
+  SerialUSB.begin(9600);
+  TempZero.init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float temperature = TempZero.readInternalTemperature();
+  SerialUSB.print("Internal Temperature is:");
+  SerialUSB.println(temperature);
+  delay(500);
+}
